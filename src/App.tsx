ae75b9582b62c1d2ddcc1f0953b71c87b3d02956
Hemant Bhardwaj <bhardwajhemant84@gmail.com>
2023-05-04 22:14:28 -0400

    Add navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import Navbar from "@/scenes/navbar";
+import { useEffect, useState } from "react";
+import { SelectedPage } from "@/shared/types";
+
 function App() {
-  return <div className="app">app</div>;
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  return (
+    <div className="app bg-gray-20">
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+    </div>
+  );
 }
 
 export default App;
