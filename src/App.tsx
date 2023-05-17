c28b308fec95db137b7043448deb58182da57cc3
Hemant Bhardwaj <bhardwajhemant84@gmail.com>
2023-05-17 13:57:20 -0400

    Add footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
 import Navbar from "@/scenes/navbar";
-import { useState } from "react";
+import Home from "@/scenes/home";
+import OurClasses from "@/scenes/ourClasses";
+import Benefits from "@/scenes/benefits";
+import ContactUs from "@/scenes/contactUs";
+import Footer from "@/scenes/footer";
+import { useEffect, useState } from "react";
 import { SelectedPage } from "@/shared/types";
-import Home from "./scenes/navbar/home";
-import Benefits from "./scenes/navbar/benefits";
-import OurClasses from "./scenes/navbar/ourClasses";
-import ContactUs from "./scenes/navbar/contactUs";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
     SelectedPage.Home
   );
   const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
     <div className="app bg-gray-20">
@@ -23,6 +36,7 @@ function App() {
       <Benefits setSelectedPage={setSelectedPage} />
       <OurClasses setSelectedPage={setSelectedPage} />
       <ContactUs setSelectedPage={setSelectedPage} />
+      <Footer />
     </div>
   );
 }
